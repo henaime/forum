acbf43f35da82d4f1d27a31723f9b31c54d9cfb3
Mourad Chairi <33106737+ourad@users.noreply.github.com>
2018-05-29 23:26:10 +0000

    mourad

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -3,25 +3,27 @@
 @section('content')
 
 
-<div class="jumbotron" style="background-color: #FFFFF0">
+<div class="jumbotron" style="background-color: #F0F0F0">
 
         <center>
           <div class="parent">
-            <img src="/storage/cover.jpg" id="cover" width="728" height="269" class="img-fluid" alt="Responsive image">
-            <a href="/profile"><img id="profile" src="/storage/photo.jpg" name="aboutme" width="140" height="140" class="img-circle"></a>
+            <img src="../{{$user->cover}}" id="cover" width="728" height="269" class="img-fluid" alt="Responsive image">
+            <a href="/profile"><img id="profile" src="../{{$user->image}}" name="aboutme" width="140" height="140" class="img-circle"></a>
           </div>
         <div>
           <br><br><br>
-          <h3>{{ $user['name'] }}</h3>
-          <h4>{{ $user['email'] }}</h4>
+          <h3>{{ $user->name }}</h3>
+          <h4>{{ $user->email }}</h4>
           <em>welcome to your profile</em>
+          <br><a href="{{url('profile/edit/'.$user->id)}}" class="btn btn-success">Change Profile</a>
+
         </div>
     </center>
 
 </div>
 <div class="jumbotron" style="background-color: #FFFFF0">
   <!-- creer un nouveau post -->
-  {!! Form::open(['action' => 'postsController@store','method'=>'POST','files'=>'true','enctype'=>'multipart/form-data',]) !!}
+  {!! Form::open(['action' => 'postsController@store','method'=>'POST']) !!}
     <div class="form-group">
       {{Form::label('title','Titre')}}
       {{Form::text('title','',['class'=>'form-control','placeholder'=>'titre'])}}
@@ -30,12 +32,10 @@
       {{Form::label('content','Contenu')}}
       {{Form::textarea('content','',['class'=>'form-control','placeholder'=>'contenu'])}}
     </div>
-    {{Form::file('photo',['class'=>'btn btn-default'])}}
     {{Form::submit('poster',['class'=>'btn btn-success'])}}
   {!! Form::close() !!}
 </div>
-  <!-- l'affichage des posts -->
-    @foreach($user['posts'] as $post)
+    @foreach($posts as $post)
      <div class="jumbotron text-left" style="background-color: #FFFFF0">
             <H3><a href="posts/{{$post->id_p}}">{{ $post->title }}</a>
             <small><a href="users/{{$post->id_user}}">(@ {{ auth()->user()->name }} )</a></small>
@@ -44,8 +44,8 @@
             <hr>
         <div>
         <a href="#"><button class="btn btn-success">j'aime </button></a>
-        <a href="/posts/{{$post->id_p}}"><button id="send" class="btn btn-primary">commenter</button></a>
-        <a href="/posts/delete/{{$post->id_p}}"><button class="btn btn-danger">supprimer post</button></a>
+        <a href="/posts/{{$post->id_p}}"><button class="btn btn-primary">commenter</button></a>
+        <a href="#"><button class="btn btn-danger">supprimer post</button></a>
         </div>
        </div>
       @endforeach
@@ -71,5 +71,3 @@
       left: 440px;
     }
 </style>
-
-
